aa062de42c63bac7e3ec57c99a73b3c0355709f9
NitinChaudhary0110 <chaudharynitin7535@gmail.com>
2025-04-24 08:36:13 +0530

    primitive data type

--- app.java
+++ app.java
@@ -1,6 +1,49 @@
 
 public class app {
     public static void main(String[] args) {
-        System.out.println("hello world !");
+        //Primitive data type in java
+
+
+        //byte -> 1byte
+
+         byte a=10;
+         System.out.println(a);
+
+        //short
+
+        short b=20;
+        System.out.println(b);
+
+        //int 
+
+        int c=30;
+        System.out.println(c);
+
+        //long
+
+        long d=40l;
+        System.out.println(d);
+
+        //float
+
+        float e=50.0f;
+        System.out.println(e);
+
+        //double
+
+        double f=60.0d;
+        System.out.println(f);
+        
+        //char
+
+        char g='A';
+        System.out.println(g);
+
+        //boolean
+
+        boolean h=true;
+        System.out.println(h);
+
+
     }
 }
